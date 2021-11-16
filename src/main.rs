f9a89bcbeff914eeadbe25348a982c0c125a9fe8
querter.ovutem@gmail.com <querter.ovutem@gmail.com>
2021-11-16 15:03:02 +0100

    Add test generation

--- src/main.rs
+++ src/main.rs
@@ -63,6 +63,36 @@ fn main() {
                 .status()
                 .expect("Failed to generate files");
         },
+        "generate" => {
+            if args.len() <= 2 {
+                throw_lerror("Not enough arguments provided!");
+            }
+
+            let limit: i32 = args[2].parse().unwrap();
+            for i in (0..limit) {
+                Command::new("g++")
+                    .arg("generate_tests.cpp")
+                    .arg("-o")
+                    .arg("gent")
+                    .status()
+                    .expect("Failed to generate testcase :(");
+
+                let mut outp = Command::new("./gent");
+
+                let mut temp = outp.output().expect("uwu");
+                let mut stdout = String::from_utf8(temp.stdout).unwrap();
+
+                fs::write(format!("tests/{}.out", i), stdout);
+
+                Command::new("rm")
+                    .arg("-f")
+                    .arg("gent")
+                    .status()
+                    .expect("a");
+
+                println!("Generated testcase {} successfully!", i+1);
+            }
+        },
         "test" => {},
         _ => throw_lerror("Invalid argument!")
     };
