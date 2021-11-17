8b5a0dba049f8711b1f34e98c0032d69c34e27e9
querter.ovutem@gmail.com <querter.ovutem@gmail.com>
2021-11-17 12:21:07 +0100

    Add logging and add test packages

--- src/main.rs
+++ src/main.rs
@@ -51,6 +51,8 @@ fn main() {
             for file in fs::read_dir(format!("{}/oi/.oisuite/project", home)).unwrap() {
                 fs::copy(file.as_ref().unwrap().path(), format!("{}/{}", name, file.unwrap().file_name().into_string().unwrap()));
             }
+
+            fs::create_dir_all(format!("{}/tests", name));
         },
         "update" => {
             let path = env::home_dir().unwrap();
@@ -66,14 +68,15 @@ fn main() {
                 .expect("Failed to generate files");
         },
         "generate" => {
-            if args.len() <= 2 {
+            if args.len() <= 3 {
                 throw_lerror("Not enough arguments provided!");
             }
 
-            let limit: i32 = args[2].parse().unwrap();
+            let packname = &args[2];
+            let limit: i32 = args[3].parse().unwrap();
 
-            fs::remove_dir_all("tests");
-            fs::create_dir("tests");
+            fs::remove_dir_all(format!("tests/{}", packname));
+            fs::create_dir(format!("tests/{}", packname));
 
             println!("Compiling {}{}test generator{}{}...", color::Fg(color::Yellow), style::Bold, color::Fg(color::Reset), style::Reset);
 
@@ -93,7 +96,7 @@ fn main() {
                 .expect("Failed to generate testcase :(");
 
             let mut working: i32 = 0;
-            fs::write(String::from("tests/testinfo"), format!("0\n{}", limit));
+            fs::write(String::from(format!("tests/{}/testinfo", packname)), format!("0\n{}", limit));
 
             for i in (1..limit+1) {
 
@@ -102,16 +105,16 @@ fn main() {
                 let mut temp_i = outp_i.output().expect("uwu");
                 let mut stdout_i = String::from_utf8(temp_i.stdout).unwrap();
 
-                fs::write(format!("tests/{}.in", i), stdout_i);
+                fs::write(format!("tests/{}/{}.in", packname, i), stdout_i);
 
                 let mut outp_o = Command::new("./brute");
 
-                outp_o.stdin(fs::File::open(format!("tests/{}.in", i)).unwrap());
+                outp_o.stdin(fs::File::open(format!("tests/{}/{}.in", packname, i)).unwrap());
 
                 let mut temp_o = outp_o.output().expect("uwu2");
                 let mut stdout_o = String::from_utf8(temp_o.stdout).unwrap();
 
-                fs::write(format!("tests/{}.out", i), stdout_o);
+                fs::write(format!("tests/{}/{}.out", packname, i), stdout_o);
 
                 println!("{}🗸 Generated testcase {} successfully!{}", color::Fg(color::Green), i, color::Fg(color::Reset));
                 working += 1;
@@ -121,6 +124,11 @@ fn main() {
             println!("Successfully generated {}{}/{}{} testcases.", color::Fg(color::Green), working, limit, color::Fg(color::Reset));
         },
         "test" => {
+            if args.len() <= 2 {
+                throw_lerror("Not enough arguments provided!");
+            }
+
+            let packname = &args[2];
             println!("Compiling {}{}algorithm{}{}...", color::Fg(color::Yellow), style::Bold, color::Fg(color::Reset), style::Reset);
 
             Command::new("g++")
@@ -130,7 +138,7 @@ fn main() {
                 .status()
                 .expect("Failed to compile :(");
 
-            let testinfo = fs::read("tests/testinfo").unwrap();
+            let testinfo = fs::read(format!("tests/{}/testinfo", packname)).unwrap();
             let detailedinfo = String::from_utf8(testinfo).unwrap();
 
             let mut split = detailedinfo.lines();
@@ -144,6 +152,7 @@ fn main() {
             if advanced_check == &ccc {
                 let mut done = vec![false; amount as usize];
 
+                let mut log = String::from("");
                 for k in (2..amount+2) {
                     let i: usize = k as usize;
                     let req = split_v[i];
@@ -156,11 +165,11 @@ fn main() {
                     }
                     if can_exec {
                         let mut rmain = Command::new("./main");
-                        rmain.stdin(fs::File::open(format!("tests/{}.in", i-1)).unwrap());
+                        rmain.stdin(fs::File::open(format!("tests/{}/{}.in", packname, i-1)).unwrap());
                         let output = rmain.output().expect("Failed running algorithm!");
                         let stdout_output = String::from_utf8(output.stdout).unwrap();
 
-                        let expectedf = fs::read(format!("tests/{}.out", i-1)).unwrap();
+                        let expectedf = fs::read(format!("tests/{}/{}.out", packname, i-1)).unwrap();
                         let expected = String::from_utf8(expectedf).unwrap();
 
 
@@ -170,32 +179,38 @@ fn main() {
                             println!("Testcase {} succeeded!", i-1);
                         } else {
                             println!("Testcase {} failed!", i-1);
+                            log += format!("\n\nTestcase {}: Got \"{}\", expected \"{}\"", i-1, stdout_output, expected).as_str();
                         }
                     } else {
                         println!("Testcase {} skipped because testcase {} failed", i-1, req)
                     }
                 }
+                fs::write(format!("tests/{}/log", packname), log);
             } else {
+                let mut log = String::from("");
                 for k in (1..amount+1) {
                     let i: usize = k as usize;
                     let mut rmain = Command::new("./main");
-                    rmain.stdin(fs::File::open(format!("tests/{}.in", i)).unwrap());
+                    rmain.stdin(fs::File::open(format!("tests/{}/{}.in", packname, i)).unwrap());
                     let output = rmain.output().expect("Failed running algorithm!");
                     let stdout_output = String::from_utf8(output.stdout).unwrap();
 
-                    let expectedf = fs::read(format!("tests/{}.out", i)).unwrap();
+                    let expectedf = fs::read(format!("tests/{}/{}.out", packname, i)).unwrap();
                     let expected = String::from_utf8(expectedf).unwrap();
+
 
                     if stdout_output.trim() == expected.trim() {
                         println!("Testcase {} succeeded!", i);
                         passed += 1;
                     } else {
                         println!("Testcase {} failed!", i);
+                        log += format!("\n\nTestcase {}: Got \"{}\", expected \"{}\"", i-1, stdout_output, expected).as_str();
                     }
                 }
+                fs::write(format!("tests/{}/log", packname), log);
             }
             fs::remove_file("main");
-            println!("Testing ended successfully. {}/{} testcases succeeded", passed, amount);
+            println!("Testing {} ended successfully. {}/{} testcases succeeded", packname, passed, amount);
         },
         _ => throw_lerror("Invalid argument!")
     };
